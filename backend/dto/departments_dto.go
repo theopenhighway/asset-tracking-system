b2f84763ae814e3ab87f14f8414ef171fd78c6aa
theopenhighway <earleen.525190010@stu.untar.ac.id>
2025-06-26 19:36:55 +0700

    feat: added department response dto

--- backend/dto/departments_dto.go
+++ backend/dto/departments_dto.go
@@ -4,6 +4,11 @@ type CreateDepartmentRequest struct {
 	Name string `json:"name" binding:"required"`
 }
 
-type GetDepartmentbyId struct {
+type GetDepartmentbyIdRequest struct {
 	Id int `json:"id" binding:"required"`
+}
+
+type DepartmentResponse struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
