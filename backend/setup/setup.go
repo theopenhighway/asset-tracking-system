d3f0cbccadeaf1b8c6ca09826fb340dc7e8ce9e3
theopenhighway <earleen.525190010@stu.untar.ac.id>
2025-06-27 19:28:50 +0700

    feat: dept routes

--- backend/setup/setup.go
+++ backend/setup/setup.go
@@ -15,9 +15,7 @@ type HandlerOps struct {
 
 func SetupHandlers(db *sql.DB) *HandlerOps {
 	deptRepository := repository.NewDeptRepository(db)
-
 	deptService := service.NewDeptService(deptRepository)
-
 	deptHandler := handler.NewDeptHandler(deptService)
 
 	return &HandlerOps{
@@ -27,6 +25,11 @@ func SetupHandlers(db *sql.DB) *HandlerOps {
 
 func SetupRoutes(h *HandlerOps) *gin.Engine {
 	g := gin.New()
+
+	g.GET("/all-dept", h.deptHandler.GetAllDept)
+	g.GET("/dept-id/{id}", h.deptHandler.GetDeptById)
+	g.POST("/add-dept", h.deptHandler.AddDept)
+	g.PATCH("/update-dept", h.deptHandler.UpdateDept)
 
 	return g
 }
